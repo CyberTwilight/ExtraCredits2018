f91c94ded6c27ded4014b09b62b62cfb5243efe1
GDelha <bernardompg@gmail.com>
2018-08-18 14:44:10 -0300

    Primeiro concept de level design

---
+++ Assets/Scripts/ScreenShake.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+
+using System.Collections;
+
+
+
+public class CameraShake : MonoBehaviour
+
+{
+
+    // Transform of the camera to shake. Grabs the gameObject's transform
+
+    // if null.
+
+    private Transform camTransform;
+
+
+
+    // How long the object should shake for.
+
+    private float shakeDuration = 0f;
+
+
+
+    // Amplitude of the shake. A larger value shakes the camera harder.
+
+    private float shakeAmount = 0.7f;
+
+    private float decreaseFactor = 1.0f;
+
+
+
+    Vector3 originalPos;
+
+    public Transform CamTransform;
+    //public Transform camTransform;
+    public float ShakeDuration;
+    //public float shakeDuration;
+    public float ShakeAmount;
+    //public float shakeAmount;
+    public float DecreaseFactor;
+    //public float decreaseFactor;
+    public Vector3 OriginalPos;
+    //public Vector3 originalPos;
+
+    void Awake()
+
+    {
+
+        if (CamTransform == null)
+
+        {
+
+            CamTransform = GetComponent<Transform>();
+
+        }
+
+    }
+
+
+
+    void OnEnable()
+
+    {
+
+        OriginalPos = CamTransform.localPosition;
+
+    }
+
+
+
+    void Update()
+
+    {
+
+        if (ShakeDuration > 0)
+
+        {
+
+            CamTransform.localPosition = OriginalPos + Random.insideUnitSphere * ShakeAmount;
+
+
+
+            ShakeDuration -= Time.deltaTime * DecreaseFactor;
+
+        }
+
+        else
+
+        {
+
+            ShakeDuration = 0f;
+
+            CamTransform.localPosition = OriginalPos;
+
+        }
+
+    }
+
+}
