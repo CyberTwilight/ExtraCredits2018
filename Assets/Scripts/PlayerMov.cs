8291bf02e90b181930d34519ef900e0bcf423fdc
Matheus Homsi <mlhomsi@gmail.com>
2018-08-18 03:25:15 -0300

    Movimento e Morte

---
+++ Assets/Scripts/PlayerMov.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMov : MonoBehaviour
+{
+
+    public float speed;
+
+    private Rigidbody2D rb2d;      
+
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rb2d.AddForce(movement * speed);
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Death")
+        {
+            Debug.Log("Dead");
+            transform.position = new Vector3(0.0f, 0.0f, 0.0f);
+        }
+
+    }
+}
