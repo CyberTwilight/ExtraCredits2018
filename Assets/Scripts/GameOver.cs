c299b4542fca4b875ed8043c7019b856f707d224
Matheus Homsi <mlhomsi@gmail.com>
2018-08-18 04:35:12 -0300

    Win and Lose State

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour {
+
+    public GameObject GameOverText;
+    public GameObject UI;
+
+
+	void Start () {
+
+        GameOverText.SetActive(true);
+        UI.SetActive(false);
+        Time.timeScale = 0.0f;  
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
